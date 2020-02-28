ae7306e28308db14d9307243a23e5aebe6be82a7
Dairo Beltran <daiydan99@hotmail.com>
2020-02-27 22:37:29 -0500

    clase triángulo documentada

--- ProyectoFiguras/src/edu/unicundi/Triangulo.java
+++ ProyectoFiguras/src/edu/unicundi/Triangulo.java
@@ -1,11 +1,13 @@
 package edu.unicundi;
 
-
+//Clase triángulo que contiene métodos y atributos que heredan de la clase Figura, que es la clase padre
 public class Triangulo extends Figura{
     
+    //variables para clacular el area, el perímetro y demás
     double areaTrian, areaTrianRec, areaTrianEqui, perimetroTrian, perimetroTrianRec, perimetroTrianEqui;
     double longitud1, longitud2, longitud3, perimetroTriangulo;
     
+    //constructor de la clase, el cuál recibe las 6 coordenadas del triángulo
     public Triangulo(double coorX1, double coorX2, double coorX3, double coorY1, double coorY2, double coorY3) {
         this.coorX1 = coorX1;
         this.coorX2 = coorX2;
@@ -18,18 +20,24 @@ public class Triangulo extends Figura{
         this.longitud3 = longitud3;
     }
 
+    
+    //método para hallar el área del tríangulo
     @Override
     public void area() {
+        
+        //para hallar el ára del triángulo, primero se deben calcular las tres longitudes
         longitud1 = Math.sqrt(Math.pow(coorX2 - coorX1, 2) + Math.pow(coorY2 - coorY1, 2));
         longitud2 = Math.sqrt(Math.pow(coorX3 - coorX2, 2) + Math.pow(coorY3 - coorY2, 2));
         longitud3 = Math.sqrt(Math.pow(coorX3 - coorX1, 2) + Math.pow(coorY3 - coorY1, 2));
         
-        
+        //el perímetro del triángulo es la suma de sus tres magnitudes
         perimetroTrian = longitud1 + longitud2 + longitud3;
-        perimetroTriangulo = perimetroTrian/2; //perímetro 
+        
+        //para aplicar la fórmula de Heron, hay que dividir el perímetro anteriormente encontrado en dos
+        perimetroTriangulo = perimetroTrian/2; 
+        
+        //se aplica la fórmula de Heron
         areaTrian = Math.sqrt(perimetroTriangulo *(perimetroTriangulo - longitud1)*(perimetroTriangulo - longitud2)*(perimetroTriangulo - longitud3));
-        //areaTrian = Math.abs(coorX1 * (coorY2 - coorY3) + coorX2 * (coorY3 - coorY1) + coorX3 * (coorY1 - coorY2))   / 2;
-        areaTrianEqui = Math.pow(ladoA, 2) * Math.sqrt(3) / 4;
-        areaTrianRec = ladoA * ladoB / 2;
+       
     }
 }
